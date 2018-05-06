e75840ce9e8dcbface77b87a1829588157d36312
Fabiano França <email@fabianofranca.com>
2018-05-06 05:18:08 +0000

    criação de uma nova activity para testar o memory leak no glue

---
+++ app/src/main/java/com/fabianofranca/kotlinlab/GlueActivity.kt
@@ -0,0 +1,62 @@
+package com.fabianofranca.kotlinlab
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.fabianofranca.glue.BindingData
+import com.fabianofranca.glue.BindingManager
+import com.fabianofranca.glue.BindingProperty
+import com.fabianofranca.glue.binding
+import kotlinx.android.synthetic.main.activity_glue.*
+import kotlin.system.measureTimeMillis
+
+class GlueActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_glue)
+
+        val user = User()
+        user.name = "Fabiano"
+        user.email = "email@fabianofranca.com"
+
+        var time = measureTimeMillis {
+            txtName.binding(user, User::name)
+            txtEmail.binding(user, User::email)
+
+            edName1.binding(user, User::name)
+            edName2.binding(user, User::name)
+
+            edEmail.binding(user, User::email)
+        }
+
+        println("tuning: setup $time")
+
+        time = measureTimeMillis {
+            user.notifyChanges()
+        }
+
+        println("tuning: init notify $time")
+
+        btnBind.setOnClickListener {
+            val time = measureTimeMillis {
+                user.notifyChanges()
+            }
+
+            println("tuning: button notify $time")
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+}
+
+data class User(val id: Int = 0) : BindingData {
+
+    override val bindingManager: BindingManager<User> = BindingManager(this)
+
+    var name: String by BindingProperty(bindingManager, "")
+    var email: String = ""
+}
--- app/src/main/java/com/fabianofranca/kotlinlab/MainActivity.kt
+++ app/src/main/java/com/fabianofranca/kotlinlab/MainActivity.kt
@@ -3,13 +3,8 @@ package com.fabianofranca.kotlinlab
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.fabianofranca.glue.BindingData
-import com.fabianofranca.glue.BindingManager
-import com.fabianofranca.glue.BindingProperty
-import com.fabianofranca.glue.binding
 import com.fabianofranca.kotlinlab.presentation.posts.PostsActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,42 +18,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, PostsActivity::class.java))
         }
 
-        val user = User()
-        user.name = "Fabiano"
-        user.email = "email@fabianofranca.com"
-
-        var time = measureTimeMillis {
-            txtName.binding(user, User::name)
-            txtEmail.binding(user, User::email)
-
-            edName1.binding(user, User::name)
-            edName2.binding(user, User::name)
-
-            edEmail.binding(user, User::email)
-        }
-
-        println("tuning: setup $time")
-
-        time = measureTimeMillis {
-            user.notifyChanges()
-        }
-
-        println("tuning: init notify $time")
-
-        btnBind.setOnClickListener {
-            val time = measureTimeMillis {
-                user.notifyChanges()
-            }
-
-            println("tuning: button notify $time")
+        btnGlue.setOnClickListener {
+            startActivity(Intent(this, GlueActivity::class.java))
         }
     }
-}
-
-data class User(val id: Int = 0) : BindingData {
-
-    override val bindingManager: BindingManager<User> = BindingManager(this)
-
-    var name: String by BindingProperty(bindingManager, "")
-    var email: String = ""
 }
